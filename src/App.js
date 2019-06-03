f9fc5583e7dab18174664965708ae7f9834770ea
Leta Keane <letakeane+turing@gmail.com>
2019-06-03 09:49:48 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import people from './data/yearbook-data.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <h1>Turing Yearbook</h1>
+    </header>
+    // Render some components here!
     </div>
   );
 }
---
+++ src/data/yearbook-data.js
@@ -0,0 +1,14 @@
+const people = [
+  {id: 1, name: 'Travis', quote: '', superlative: '', cohort: 'staff'},
+  {id: 2, name: 'Leta', quote: '', superlative: '', cohort: 'staff'},
+  {id: 3, name: 'Eric', quote: '', superlative: '', cohort: 'staff'},
+  {id: 4, name: 'Khalid', quote: '', superlative: '', cohort: 'staff'},
+  {id: 5, name: 'Brittany', quote: '', superlative: '', cohort: 'staff'},
+  {id: 6, name: 'Robbie', quote: '', superlative: '', cohort: 'staff'},
+  {id: 7, name: 'Will', quote: '', superlative: '', cohort: 'staff'},
+  {id: 8, name: 'Pam', quote: '', superlative: '', cohort: 'staff'},
+  {id: 9, name: 'Louisa', quote: '', superlative: '', cohort: 'staff'},
+  {id: 10, name: 'Jeff', quote: '', superlative: '', cohort: 'staff'},
+];
+
+module.exports = people;
