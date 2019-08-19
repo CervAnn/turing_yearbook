d849667bad7058c69adb46a327a88a888a3b2227
Ann Cerveny <cervann11@gmail.com>
2019-08-18 21:39:01 -0600

    Complete editable info functionality

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,9 +6,10 @@ const Person = (props) => {
     return (
         <div className="Person">
             <img className="photo" alt="" src={props.photo}/>
-            <h3 contentEditable={isEditable}>{props.name}</h3>
-            <p contentEditable={isEditable}><em>{props.quote}</em></p>
-            <h3 contentEditable={isEditable}>{props.superlative}</h3>
+            <h3 contentEditable={isEditable}>{props.name === "" ? "Add Name Here" : props.name}</h3>
+            <p contentEditable={isEditable}><em>{props.quote === "" ? "Add Quote Here" : props.quote}</em></p>
+            <h3 contentEditable={isEditable}>{props.superlative === "" ? "Add Superlative Here" : props.superlative}</h3>
+            <button style={{display: isEditable ? "block" : "none"}}>Remove Student</button>
         </div>
     )
 }
