50ccdbf582e45c70e24062cdf0965bb60b62cad0
Ann Cerveny <cervann11@gmail.com>
2019-08-18 22:32:26 -0600

    Suppress contentEditable warnings

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,9 +6,9 @@ const Person = ({photo, name, title, quote, superlative, deleteStudent, id}) => 
     return (
         <div className="Person">
             <img className="photo" alt="" src={photo}/>
-            <h3 contentEditable={isEditable}>{name === "" ? "Add Name Here" : name}</h3>
-            <p contentEditable={isEditable}><em>{quote === "" ? "Add Quote Here" : `"${quote}"`}</em></p>
-            <h3 contentEditable={isEditable}>{superlative === "" ? "Add Superlative Here" : superlative}</h3>
+            <h3 contentEditable={isEditable} suppressContentEditableWarning={isEditable}>{name === "" ? "Add Name Here" : name}</h3>
+            <p contentEditable={isEditable} suppressContentEditableWarning={isEditable}><em>{quote === "" ? "Add Quote Here" : `"${quote}"`}</em></p>
+            <h3 contentEditable={isEditable} suppressContentEditableWarning={isEditable}>{superlative === "" ? "Add Superlative Here" : superlative}</h3>
             <button style={{display: isEditable ? "block" : "none"}} onClick={(e) => deleteStudent(id)}>Remove Student</button>
         </div>
     )
