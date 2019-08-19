6df2a52acf1e3e6ba29bbb025ab75bce0e26d32b
Ann Cerveny <cervann11@gmail.com>
2019-08-19 13:53:06 -0600

    Disable Autocomplete on Form

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -33,9 +33,9 @@ class StudentForm extends Component {
     render() {
         return (
             <form className="StudentForm">
-                <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/>
-                <input type="text" placeholder="Quote" name="quote" value={this.state.quote} onChange={this.handleChange}/>
-                <input type="text" placeholder="Most Likey to..." name="superlative" value={this.state.superlative} onChange={this.handleChange}/>
+                <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange} autocomplete="off"/>
+                <input type="text" placeholder="Quote" name="quote" value={this.state.quote} onChange={this.handleChange} autocomplete="off"/>
+                <input type="text" placeholder="Most Likey to..." name="superlative" value={this.state.superlative} onChange={this.handleChange} autocomplete="off"/>
                 <button className="formButton" onClick={e => this.handleSubmit(e)}>Add Student</button>
             </form>
         )
