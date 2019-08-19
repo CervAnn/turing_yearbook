f1cfa0d3871549ed79b882e8324c975e6bfd552a
Ann Cerveny <cervann11@gmail.com>
2019-08-18 22:01:13 -0600

    Add deleteStudent functionality

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-const Person = (props) => {
-    const isEditable = (props.title === "Students" ? true : false);
+const Person = ({photo, name, title, quote, superlative, deleteStudent, id}) => {
+    const isEditable = (title === "Students" ? true : false);
     return (
         <div className="Person">
-            <img className="photo" alt="" src={props.photo}/>
-            <h3 contentEditable={isEditable}>{props.name === "" ? "Add Name Here" : props.name}</h3>
-            <p contentEditable={isEditable}><em>{props.quote === "" ? "Add Quote Here" : props.quote}</em></p>
-            <h3 contentEditable={isEditable}>{props.superlative === "" ? "Add Superlative Here" : props.superlative}</h3>
-            <button style={{display: isEditable ? "block" : "none"}}>Remove Student</button>
+            <img className="photo" alt="" src={photo}/>
+            <h3 contentEditable={isEditable}>{name === "" ? "Add Name Here" : name}</h3>
+            <p contentEditable={isEditable}><em>{quote === "" ? "Add Quote Here" : `"${quote}"`}</em></p>
+            <h3 contentEditable={isEditable}>{superlative === "" ? "Add Superlative Here" : superlative}</h3>
+            <button style={{display: isEditable ? "block" : "none"}} onClick={(e) => deleteStudent(id)}>Remove Student</button>
         </div>
     )
 }
