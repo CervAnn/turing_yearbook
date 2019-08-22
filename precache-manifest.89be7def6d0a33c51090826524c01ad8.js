5da92dbc1a480bcf645a6bd176f4daa3328c6185
Ann Cerveny <cervann11@gmail.com>
2019-08-22 07:32:53 -0600

    Updates

---
+++ precache-manifest.89be7def6d0a33c51090826524c01ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb6f1126a4bf19dee7aa97c55eef128",
+    "url": "/turing_yearbook/index.html"
+  },
+  {
+    "revision": "41eed0e7fa5aff400750",
+    "url": "/turing_yearbook/static/css/main.b5184fde.chunk.css"
+  },
+  {
+    "revision": "8587b6ef486c15029975",
+    "url": "/turing_yearbook/static/js/2.3adfd0b1.chunk.js"
+  },
+  {
+    "revision": "41eed0e7fa5aff400750",
+    "url": "/turing_yearbook/static/js/main.8b55272f.chunk.js"
+  },
+  {
+    "revision": "fb4cabbe3b6117133299",
+    "url": "/turing_yearbook/static/js/runtime~main.f438c2ea.js"
+  }
+]);
